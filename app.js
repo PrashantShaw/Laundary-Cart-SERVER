2eb09bfb8ff675bfaa363ff0bd7acf3f05515aa2
PrashantShaw <prashant.shaw.3154@gmail.com>
2022-11-24 14:46:16 +0530

    BAckend part completed

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express')
+const authorization = require('./src/auth/auth.js')
+const loginRoutes = require('./src/routes/loginRoutes')
+const orderRoutes = require('./src/routes/orderRoutes')
+
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use('/order', authorization)
+
+
+
+app.use(loginRoutes)
+app.use('/order', orderRoutes)
 
 
 app.get('/', (req, res) => {
     res.send("All Ok.")
 })
-
-
-
-
-
 
 
 
